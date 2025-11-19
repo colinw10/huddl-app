5d1c3e70e14c2bc577921e6b6a15f20ca607a8c1
Pablo Cordero <cordero080@gmail.com>
2025-11-18 23:50:06 -0500

    Add page components (Feed, Login, SignUp, Profile) and install react-router-dom

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+function Profile () {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>User profile will go here</p>
+    </div>
+  );
+}
+
+export default Profile;
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+function Signup (){
+  return (
+    <div>
+      <h1> Sign Up</h1>
+      <p>Create your account</p>
+    </div>
+  );
+}
+
+export default Signup;
